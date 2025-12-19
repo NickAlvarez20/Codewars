8f3f0174edca8dff65347daa6ae19151385efee0
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2025-12-18 21:14:57 -0700

    solve: Solve filter geese using some and lowerCase checks

---
+++ TypeScript/filterGeese.ts
@@ -0,0 +1,16 @@
+export function gooseFilter(birds: string[]): string[] {
+  const geese: string[] = [
+    "African",
+    "Roman Tufted",
+    "Toulouse",
+    "Pilgrim",
+    "Steinbacher",
+  ];
+  let notGeeseArr: string[] = [];
+  for (const bird of birds) {
+    if (!geese.some((goose) => goose.toLowerCase() === bird.toLowerCase())) {
+      notGeeseArr.push(bird);
+    }
+  }
+  return notGeeseArr;
+}
